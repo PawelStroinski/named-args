099eb03424697a4b2c52deb2b05fd91bb025f9d7
Paweł Stroiński <pol84@live.com>
2015-05-31 23:46:15 +0100

    More options from defn signature supported by defnam

--- src/named/args.clj
+++ src/named/args.clj
@@ -1,6 +1,7 @@
 (ns named.args
   (require [clojure.string :as str]
-           [clojure.set :refer [difference]]))
+           [clojure.set :refer [difference]]
+           [clojure.core.match :refer [match]]))
 
 (defn check-arguments [declared supplied]
   (let [suppl (set (keys supplied))
@@ -16,16 +17,37 @@
       (not-empty superfluous) (error "Superfluous arguments: %s" superfluous)
       :else true)))
 
-(defmacro defnam [name args body]
-  `(defn ~name [& {:keys ~args
-                   :as   ~'arguments}]
-     {:pre [(check-arguments ~(set (map keyword args)) ~'arguments)]}
-     ~body))
+(defn- params [args arguments]
+  ['& {:keys args :as arguments}])
 
-(comment
-  (defnam foo [bar1 bar2] arguments)
-  (foo :bar1 1 :bar2 2 :bar3 3)
-  (foo :bar1 1 :bar2 2)
-  (foo :bar1 1)
-  (macroexpand-1 '(defnam foo [bar1] arguments))
-  )
+(defn- prepost-map [body]
+  (let [candidate (first body)]
+    (when (and (or (:pre candidate) (:post candidate))
+               (next body))
+      candidate)))
+
+(defn- pre [args arguments body]
+  (let [pre1 {:pre [(list 'named.args/check-arguments (set (map keyword args)) arguments)]}
+        prepost-map (prepost-map body)]
+    (if prepost-map
+      (merge-with (comp vec concat) pre1 prepost-map)
+      pre1)))
+
+(defn- dobody [body]
+  (if (prepost-map body)
+    (rest body)
+    body))
+
+(defmacro defnam
+  "Similar to defn but all args of the function defined are named and non-optional.
+   Custom argument destructuring, variadic args & overloads are not supported."
+  {:arglists '([name doc-string? attr-map? [params*] prepost-map? body])}
+  [name & spec]
+  (let [arguments (gensym "arguments")]
+    (match (vec spec)
+           [[& args] & body] `(defn ~name ~(params args arguments) ~(pre args arguments body) ~@(dobody body))
+           [doc-string-or-attr-map [& args] & body] `(defn ~name ~doc-string-or-attr-map ~(params args arguments)
+                                                       ~(pre args arguments body) ~@(dobody body))
+           [doc-string attr-map [& args] & body] `(defn ~name ~doc-string ~attr-map ~(params args arguments)
+                                                    ~(pre args arguments body) ~@(dobody body))
+           )))
--- test/named/args_test.clj
+++ test/named/args_test.clj
@@ -1,18 +1,89 @@
 (ns named.args-test
   (:require [clojure.test :refer :all]
-            [named.args :refer :all])
+            [named.args :refer [defnam]])
   (:import (clojure.lang ExceptionInfo)))
+
+(defn check-doc-string [foo]
+  (is (= "foodoc" (:doc (meta foo)))))
+
+(defn check-attr-map [foo]
+  (is (= "foometa" (:foometa (meta foo)))))
+
+(defn check-pre-in-prepost-map [foo]
+  (is (thrown-with-msg? AssertionError #"10" (foo :bar1 10 :bar2 0))))
+
+(defn check-post-in-prepost-map [foo]
+  (is (thrown-with-msg? AssertionError #"20" (foo :bar1 0 :bar2 20))))
+
+(defn check-not-prepost-map [foo]
+  (is (= {:pre [false] :post [false]} (foo :bar1 1 :bar2 2))))
 
 (defn foo-fixture [f]
   (defnam foo [bar1 bar2]
-          arguments)
+          (+ bar1 bar2))
   (f)
-  (ns-unmap 'named.args-test 'foo))
+
+  (defnam foo [bar1 bar2]
+          (let [])
+          (+ bar1 bar2))
+  (f)
+
+  (defnam foo
+          "foodoc"
+          [bar1 bar2]
+          (let [])
+          (+ bar1 bar2))
+  (f)
+  (check-doc-string #'foo)
+
+  (defnam foo
+          {:foometa "foometa"}
+          [bar1 bar2]
+          (let [])
+          (+ bar1 bar2))
+  (f)
+  (check-attr-map #'foo)
+
+  (defnam foo
+          "foodoc"
+          {:foometa "foometa"}
+          [bar1 bar2]
+          (let [])
+          (+ bar1 bar2))
+  (f)
+  (check-doc-string #'foo)
+  (check-attr-map #'foo)
+
+  (defnam foo [bar1 bar2]
+          {:pre [(< bar1 10)]}
+          (let [])
+          (+ bar1 bar2))
+  (f)
+  (check-pre-in-prepost-map foo)
+
+  (defnam foo
+          "foodoc"
+          {:foometa "foometa"}
+          [bar1 bar2]
+          {:post [(< % 20)]}
+          (let [])
+          (+ bar1 bar2))
+  (f)
+  (check-doc-string #'foo)
+  (check-attr-map #'foo)
+  (check-post-in-prepost-map foo)
+
+  (defnam foo [bar1 bar2]
+          {:pre [false] :post [false]})
+  (check-not-prepost-map foo)
+
+  (ns-unmap 'named.args-test 'foo)
+  )
 
 (use-fixtures :once foo-fixture)
 
 (deftest defnam-test
   (is (thrown-with-msg? ExceptionInfo #"(?i)missing arguments: bar2" (foo :bar1 1)))
-  (is (= {:bar1 1 :bar2 2} (foo :bar1 1 :bar2 2)))
+  (is (= 3 (foo :bar1 1 :bar2 2)))
   (is (thrown-with-msg? ExceptionInfo #"(?i)superfluous arguments: bar3" (foo :bar1 1 :bar2 2 :bar3 3)))
   )
